410d952b4a8f506004d6b4f6096ff5ef969df1fa
Arjun <arjuntvd@gmail.com>
2015-07-17 18:14:14 -0400

    first line\nsecond line

--- code.C
+++ code.C
@@ -5,4 +5,5 @@ int main(){
   cout << "Hello, World!" << endl;
   cout << "This line was added as a new feature that was developed in Branch 'master_test-new-feature' and after testing was merged into Branch 'master'" << endl;
   cout <<"Line added in Branch iss1"
+  cout <<"Testing multiline commit"<<endl;
 }
